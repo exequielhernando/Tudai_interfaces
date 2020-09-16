8ad6078dafbaeb1dcb5499d208af44b22cc07150
Exequiel Hernando <exequielhernando@gmail.com>
2020-09-15 23:19:52 -0300

    Trabaja practico entregable

---
+++ Tp1/EntregablePractico1/js/main.js
@@ -0,0 +1,72 @@
+"use strict"
+window.addEventListener("load", () =>{
+
+    //start variables
+    let canvas = document.querySelector('#canvas');
+    let context = canvas.getContext('2d');
+    let newFile = document.querySelector("#newFile");
+    let pencil = document.querySelector("#pencil");
+    let eraser = document.querySelector("#eraser");
+    let drawing = false;
+    let erasing = false;
+
+    // clear canvas
+    const newCanvas = () => {
+        context.fillStyle = "#fff"; // canvas background color
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        context.beginPath();
+    }
+   
+    //Mouse events
+    const mouseEvents = () => {
+        canvas.addEventListener("mousedown", startDraw, false);
+        canvas.addEventListener("mouseup", finishDraw, false);
+        canvas.addEventListener("mousemove", drawLine, false);
+    }
+    //Begin to draw
+    const startDraw = e => {
+        drawing = true;
+        drawLine(e);
+    }
+    //Finish to draw
+    const finishDraw = () => {
+        drawing = false;
+        context.beginPath();
+    }
+    //Draw a line
+    const drawLine = e => {
+        e.preventDefault();
+        if (erasing) context.strokeStyle = "#fff";
+        if (drawing) {
+            // Estilos de linea
+            context.lineJoin = context.lineCap = 'round';
+            context.lineWidth = document.querySelector("#pencil-size").value;
+            // Redibuja todas las lineas guardadas
+            context.beginPath();
+            context.moveTo(e.clientX, e.clientY);
+            context.lineTo(e.clientX, e.clientY);
+            context.stroke();
+        }
+       
+    }
+    // Enable pencil to draw
+    const enablePencil = () => {
+        context.strokeStyle = "#000";
+        context.lineCap = "round";
+        erasing = false;
+        mouseEvents();
+    }
+    //Enable eraser to erase
+    const enableEraser = () => {
+        context.strokeStyle = "#fff";
+        context.lineCap = "round";
+        erasing = true;
+        mouseEvents();
+    }
+    
+    //Event Listeners
+    newFile.addEventListener("click", newCanvas);
+    pencil.addEventListener("click", enablePencil);
+    eraser.addEventListener("click", enableEraser);
+
+})
